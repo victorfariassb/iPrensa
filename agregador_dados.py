355cdeafc6b6008c47c7c5daccab6c65dbb11883
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2021-12-21 17:46:26 -0300

    transforma em funções

--- agregador_dados.py
+++ agregador_dados.py
@@ -7,41 +7,31 @@
 import pandas as pd
 import glob
 
+def planilha_globo():
+    arquivos_globo = []
+    for arquivo in glob.glob('C:\\Users\\victo\\OneDrive\\Documentos\\Master\\analise_jornais\\globo*csv'):
+        arquivos_globo.append(arquivo)
 
-# In[2]:
+    tabelas_globo = []
+    for arquivo in arquivos_globo:
+        tabelas_globo.append(pd.read_csv(arquivo, index_col=0))
 
+    arq_fim_globo = pd.concat(tabelas_globo).reset_index()
+    arq_fim_globo = arq_fim_globo.rename(columns={'0': 'data', '1': 'editoria', '2': 'posicao', '3': 'titulo', '4': 'link'})
+    return arq_fim_globo
 
-arquivos_globo = []
-for arquivo in glob.glob('C:\\Users\\victo\\OneDrive\\Documentos\\Master\\analise_jornais\\globo*csv'):
-    arquivos_globo.append(arquivo)
+def planilha_uol():
+    arquivos_uol = []
+    for arquivo in glob.glob('C:\\Users\\victo\\OneDrive\\Documentos\\Master\\analise_jornais\\uol*csv'):
+        arquivos_uol.append(arquivo)
 
-tabelas_globo = []
-for arquivo in arquivos_globo:
-    tabelas_globo.append(pd.read_csv(arquivo, index_col=0))
+    tabelas_uol = []
+    for arquivo in arquivos_uol:
+        tabelas_uol.append(pd.read_csv(arquivo, index_col=0))
 
-arq_fim_globo = pd.concat(tabelas_globo).reset_index()
-arq_fim_globo = arq_fim_globo.rename(columns={'0': 'data', '1': 'editoria', '2': 'posicao', '3': 'titulo', '4': 'link'})
-arq_fim_globo
-
-
-# In[3]:
-
-
-arquivos_uol = []
-for arquivo in glob.glob('C:\\Users\\victo\\OneDrive\\Documentos\\Master\\analise_jornais\\uol*csv'):
-    arquivos_uol.append(arquivo)
-
-tabelas_uol = []
-for arquivo in arquivos_uol:
-    tabelas_uol.append(pd.read_csv(arquivo, index_col=0))
-
-arq_fim_uol = pd.concat(tabelas_uol).reset_index()
-arq_fim_uol = arq_fim_uol.rename(columns={'0': 'data', '1': 'classe', '2': 'titulo', '3': 'link'})
-arq_fim_uol
-
-
-# In[ ]:
-
+    arq_fim_uol = pd.concat(tabelas_uol).reset_index()
+    arq_fim_uol = arq_fim_uol.rename(columns={'0': 'data', '1': 'classe', '2': 'titulo', '3': 'link'})
+    return arq_fim_uol
 
 
 
