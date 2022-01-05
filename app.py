6079066f0a9046f5d9d6408bf4213ba27267dbe9
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-05 13:40:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -68,9 +68,22 @@ def dados_candidatos():
     semana_moro, mes_moro, ano_moro = contagem_candidatos('Moro', uol)
     semana_ciro, mes_ciro, ano_ciro = contagem_candidatos('Ciro', uol)
     semana_doria, mes_doria, ano_doria = contagem_candidatos('Doria', uol)
+    semana_bolso_globo, mes_bolso_globo, ano_bolso_globo = contagem_candidatos('Bolsonaro', globo)
+    semana_lula_globo, mes_lula_globo, ano_lula_globo = contagem_candidatos('Lula', globo)
+    semana_moro_globo, mes_moro_globo, ano_moro_globo = contagem_candidatos('Moro', globo)
+    semana_ciro_globo, mes_ciro_globo, ano_ciro_globo = contagem_candidatos('Ciro', globo)
+    semana_doria_globo, mes_doria_globo, ano_doria_globo = contagem_candidatos('Doria', globo)
     return render_template(
         "home.html", semana_bolso=semana_bolso, mes_bolso=mes_bolso, ano_bolso=ano_bolso,
         semana_lula=semana_lula, mes_lula=mes_lula, ano_lula=ano_lula,
         semana_moro=semana_moro, mes_moro=mes_moro, ano_moro=ano_moro,
         semana_ciro=semana_ciro, mes_ciro=mes_ciro, ano_ciro=ano_ciro,
-        semana_doria=semana_doria, mes_doria=mes_doria, ano_doria=ano_doria)
+        semana_doria=semana_doria, mes_doria=mes_doria, ano_doria=ano_doria
+        semana_bolso_globo=semana_bolso_globo, mes_bolso_globo=mes_bolso_globo, ano_bolso_globo=ano_bolso_globo
+        semana_lul_globoa=semana_lul_globoa, mes_lula_globo=mes_lula_globo, ano_lula_globo=ano_lula_globo,
+        semana_moro_globo=semana_moro_globo, mes_moro_globo=mes_moro_globo, ano_moro_globo=ano_moro_globo,
+        semana_ciro_globo=semana_ciro_globo, mes_ciro_globo=mes_ciro_globo, ano_ciro_globo=ano_ciro_globo,
+        semana_doria_globo=semana_doria_globo, mes_doria_globo=mes_doria_globo, ano_doria_globo=ano_doria_globo)
+
+
+)
