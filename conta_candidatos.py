761a5e74c5b3e755b917b3f6b65fa65826d6f48e
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-09 14:49:14 -0300

    Update conta_candidatos.py

--- conta_candidatos.py
+++ conta_candidatos.py
@@ -1,39 +1,50 @@
+import gspread
 import pandas as pd
 import datetime
 import numpy as np
 
 
 def contagem_candidatos(termo, dataframe):
-    hoje = datetime.datetime.now()
-    semana = hoje - datetime.timedelta(days=7, hours=3)
-    semana = np.datetime64(semana)
+    linha = 2
+    coluna = 2
 
-    dataframe['data'] = pd.to_datetime(dataframe['data'])
-    dataframe['titulo'] = dataframe['titulo'].str.replace('Carlos Bolsonaro', 'Carlos B.')
-    dataframe['titulo'] = dataframe['titulo'].str.replace('Flávio Bolsonaro', 'Flávio B.')
-    dataframe['titulo'] = dataframe['titulo'].str.replace('Eduardo Bolsonaro', 'Eduardo B.')
+    presidenciaveis = ['Bolsonaro', 'Lula', 'Moro', 'Ciro', 'Doria', 'Pacheco', 'Tebet', 'Vieira']
+    for presidenciavel in presidenciaveis:
+        
+        hoje = datetime.datetime.now()
+        semana = hoje - datetime.timedelta(days=7, hours=3)
+        semana = np.datetime64(semana)
 
-    df_semana = dataframe[dataframe['data'] >= semana]
+        dataframe['data'] = pd.to_datetime(dataframe['data'])
+        dataframe['titulo'] = dataframe['titulo'].str.replace('Carlos Bolsonaro', 'Carlos B.')
+        dataframe['titulo'] = dataframe['titulo'].str.replace('Flávio Bolsonaro', 'Flávio B.')
+        dataframe['titulo'] = dataframe['titulo'].str.replace('Eduardo Bolsonaro', 'Eduardo B.')
 
-    termo_semana = df_semana[['link']][df_semana['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
-    termo_semana = len(termo_semana)
+        df_semana = dataframe[dataframe['data'] >= semana]
 
-    # Mes
-    mes = hoje - datetime.timedelta(days=30, hours=3)
-    mes = np.datetime64(mes)
+        termo_semana = df_semana[['link']][df_semana['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
+        termo_semana = len(termo_semana)
 
-    df_mes = dataframe[dataframe['data'] >= mes]
+        # Mes
+        mes = hoje - datetime.timedelta(days=30, hours=3)
+        mes = np.datetime64(mes)
 
-    termo_mes = df_mes[['link']][df_mes['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
-    termo_mes = len(termo_mes)
+        df_mes = dataframe[dataframe['data'] >= mes]
 
-    # Ano
-    ano = datetime.datetime(2022, 1, 1, 0, 0, 0)
+        termo_mes = df_mes[['link']][df_mes['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
+        termo_mes = len(termo_mes)
 
-    df_ano = dataframe[dataframe['data'] >= ano]
+        # Ano
+        ano = datetime.datetime(2022, 1, 1, 0, 0, 0)
 
-    termo_ano = df_ano[['link']][df_ano['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
-    termo_ano = len(termo_ano)
+        df_ano = dataframe[dataframe['data'] >= ano]
 
-    return termo_semana, termo_mes, termo_ano
+        termo_ano = df_ano[['link']][df_ano['titulo'].str.contains(f'{termo}')].drop_duplicates().reset_index(drop=True)
+        termo_ano = len(termo_ano)
+
+        worksheet2 = spreadsheet.worksheet(f'{pagina}')
+        worksheet2.update_cell(linha, coluna, termo_semana)
+        worksheet2.update_cell(linha, coluna + 1, termo_mes)
+        worksheet2.update_cell(linha, coluna + 2, termo_ano)
+        linha += 1
 
