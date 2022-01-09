beea1eb8e01d214baf296063efba79cdf49e7ecf
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-09 15:05:38 -0300

    coleta de dados do sheets

--- app.py
+++ app.py
@@ -71,32 +71,68 @@ g8=palavras_globo[7][1]
 g9=palavras_globo[8][1] 
 g10=palavras_globo[9][1] 
 
+worksheet3 = spreadsheet.worksheet('contagem_globo')
+contagem_globo = pd.DataFrame(worksheet3.get_all_records())
+
+gb1 = contagem_globo.acell('B2').value
+gb2 = contagem_globo.acell('C2').value
+gb3 = contagem_globo.acell('D2').value
+
+gl1 = contagem_globo.acell('B3').value
+gl2 = contagem_globo.acell('C3').value
+gl3 = contagem_globo.acell('D3').value
+
+gm1 = contagem_globo.acell('B4').value
+gm2 = contagem_globo.acell('C4').value
+gm3 = contagem_globo.acell('D4').value
+
+gc1 = contagem_globo.acell('B5').value
+gc2 = contagem_globo.acell('C5').value
+gc3 = contagem_globo.acell('D5').value
+
+gd1 = contagem_globo.acell('B6').value
+gd2 = contagem_globo.acell('C6').value
+gd3 = contagem_globo.acell('D6').value
+
+worksheet4 = spreadsheet.worksheet('contagem_uol')
+contagem_uol = pd.DataFrame(worksheet4.get_all_records())
+
+ub1 = contagem_uol.acell('B2').value
+ub2 = contagem_uol.acell('C2').value
+ub3 = contagem_uol.acell('D2').value
+
+ul1 = contagem_uol.acell('B3').value
+ul2 = contagem_uol.acell('C3').value
+ul3 = contagem_uol.acell('D3').value
+
+um1 = contagem_uol.acell('B4').value
+um2 = contagem_uol.acell('C4').value
+um3 = contagem_uol.acell('D4').value
+
+uc1 = contagem_uol.acell('B5').value
+uc2 = contagem_uol.acell('C5').value
+uc3 = contagem_uol.acell('D5').value
+
+ud1 = contagem_uol.acell('B6').value
+ud2 = contagem_uol.acell('C6').value
+ud3 = contagem_uol.acell('D6').value
+
 
 @app.route("/")
 def dados_candidatos():
-    semana_bolso, mes_bolso, ano_bolso = contagem_candidatos('Bolsonaro', uol)
-    semana_lula, mes_lula, ano_lula = contagem_candidatos('Lula', uol)
-    semana_moro, mes_moro, ano_moro = contagem_candidatos('Moro', uol)
-    semana_ciro, mes_ciro, ano_ciro = contagem_candidatos('Ciro', uol)
-    semana_doria, mes_doria, ano_doria = contagem_candidatos('Doria', uol)
-    semana_bolso_globo, mes_bolso_globo, ano_bolso_globo = contagem_candidatos('Bolsonaro', globo)
-    semana_lula_globo, mes_lula_globo, ano_lula_globo = contagem_candidatos('Lula', globo)
-    semana_moro_globo, mes_moro_globo, ano_moro_globo = contagem_candidatos('Moro', globo)
-    semana_ciro_globo, mes_ciro_globo, ano_ciro_globo = contagem_candidatos('Ciro', globo)
-    semana_doria_globo, mes_doria_globo, ano_doria_globo = contagem_candidatos('Doria', globo)
     return render_template(
         "home.html",
         w1=w1, w2=w2, w3=w3, w4=w4, w5=w5, w6=w6, w7=w7, w8=w8, w9=w9, w10=w10,
         g1=g1, g2=g2, g3=g3, g4=g4, g5=g5, g6=g6, g7=g7, g8=g8, g9=g9, g10=g10,
         p1=p1, p2=p2, p3=p3, p4=p4, p5=p5, p6=p6, p7=p7, p8=p8, p9=p9, p10=p10,
         u1=u1, u2=u2, u3=u3, u4=u4, u5=u5, u6=u6, u7=u7, u8=u8, u9=u9, u10=u10,
-        semana_bolso=semana_bolso, mes_bolso=mes_bolso, ano_bolso=ano_bolso,
-        semana_lula=semana_lula, mes_lula=mes_lula, ano_lula=ano_lula,
-        semana_moro=semana_moro, mes_moro=mes_moro, ano_moro=ano_moro,
-        semana_ciro=semana_ciro, mes_ciro=mes_ciro, ano_ciro=ano_ciro,
-        semana_doria=semana_doria, mes_doria=mes_doria, ano_doria=ano_doria,
-        semana_bolso_globo=semana_bolso_globo, mes_bolso_globo=mes_bolso_globo, ano_bolso_globo=ano_bolso_globo,
-        semana_lula_globo=semana_lula_globo, mes_lula_globo=mes_lula_globo, ano_lula_globo=ano_lula_globo,
-        semana_moro_globo=semana_moro_globo, mes_moro_globo=mes_moro_globo, ano_moro_globo=ano_moro_globo,
-        semana_ciro_globo=semana_ciro_globo, mes_ciro_globo=mes_ciro_globo, ano_ciro_globo=ano_ciro_globo,
-        semana_doria_globo=semana_doria_globo, mes_doria_globo=mes_doria_globo, ano_doria_globo=ano_doria_globo)
+        ub1=semana_bolso, ub2=mes_bolso, ub3=ano_bolso,
+        ul1=semana_lula, ul2=mes_lula, ul3=ano_lula,
+        um1=semana_moro, um2=mes_moro, um3=ano_moro,
+        uc1=semana_ciro, uc2=mes_ciro, uc3=ano_ciro,
+        ud1=semana_doria, ud2=mes_doria, ud3=ano_doria,
+        gb1=semana_bolso_globo, gb2=mes_bolso_globo, gb3=ano_bolso_globo,
+        gl1=semana_lula_globo, gl2=mes_lula_globo, gl3=ano_lula_globo,
+        gm1=semana_moro_globo, gm2=mes_moro_globo, gm3=ano_moro_globo,
+        gc1=semana_ciro_globo, gc2=mes_ciro_globo, gc3=ano_ciro_globo,
+        gd1=semana_doria_globo, gd2=mes_doria_globo, gd3=ano_doria_globo)
