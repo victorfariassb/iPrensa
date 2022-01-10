629995637396fd834571acfbe0d9872ff80c2eab
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-10 13:51:38 -0300

    Update contagem_palavras.py

--- contagem_palavras.py
+++ contagem_palavras.py
@@ -23,7 +23,7 @@ def conta_palavras(arquivo):
 
     stop_words = set(stopwords.words("portuguese"))
     for x in ['seção','@','#',',', '!', ':', 'vídeo', 'quer', 'uol', 'vai', 'carros', 'pode', 'novo', 'afirma', '2021', 'confira', 'durante', '.', 'a', 'sobre', 'diz', 'após', 'veja', 'ser', 'faz', 'ex',
-              'r', '1', '4', '9', '2', '3', '5', '6', '7', '8', 'anos', 'ano', '2022', 'dia']:
+              'r', '1', '4', '9', '2', '3', '5', '6', '7', '8', 'anos', 'ano', '2022', 'dia', 'contra', 'virada', 'melhores']:
         stop_words.add(x)
 
     tokenized_sent = tokenized_word
