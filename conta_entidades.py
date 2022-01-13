cdcbd6f988522eb7e1448cffc7804b44d5de689f
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-13 16:00:18 -0300

    Create conta_entidades.py

---
+++ conta_entidades.py
@@ -0,0 +1,22 @@
+import spacy
+import spacy.attrs
+
+from collections import Counter, OrderedDict
+
+!python -m spacy download pt
+nlp = spacy.load('pt')
+
+def conta_entidade(df):
+  text = ''
+  for index, row in df.iterrows():
+    text = text + row['titulo'] + ' '
+        
+  doc = nlp(text)
+
+  palavras = {}
+
+  labels = [x.text for x in doc.ents]
+  dicionario = Counter(labels)
+
+  palavras = OrderedDict(sorted(dicionario.items(), key = lambda kv : kv[1], reverse=True))
+  return palavras
