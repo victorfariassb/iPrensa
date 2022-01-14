fdfc38af18d6a1bad6c4d4b073583d8d5c5e6810
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-13 22:53:44 -0300

    Update conta_entidades.py

--- conta_entidades.py
+++ conta_entidades.py
@@ -1,14 +1,34 @@
+import gspread
+import base64
+import os
+import json
+import pandas as pd
 import spacy
-import spacy.attrs
 import pt_core_news_sm
-
 from collections import Counter, OrderedDict
 
+
+spreadsheet_id = os.environ['GOOGLE_SHEET_ID']
+conteudo_codificado =  os.environ['GOOGLE_SHEETS_CREDENTIALS']
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+
+service_account = gspread.service_account_from_dict(credentials)
+spreadsheet = service_account.open_by_key(spreadsheet_id) 
+
+uol = spreadsheet.worksheet('uol')
+globo = spreadsheet.worksheet('globo')
+jp = spreadsheet.worksheet('jp')
+entidades = spreadsheet.worksheet('entidades')
+
+sites = [uol, globo, jp]
 
 nlp = pt_core_news_sm.load()
 nlp.max_length = 200000000
 
 def conta_entidade(df):
+  for site in sites:
+    df = pd.DataFrame(site.get_all_records())   
   text = ''
   for index, row in df.iterrows():
     text = text + row['titulo'] + ' '
@@ -21,4 +41,6 @@ def conta_entidade(df):
   dicionario = Counter(labels)
 
   palavras = OrderedDict(sorted(dicionario.items(), key = lambda kv : kv[1], reverse=True))
-  return palavras
+  palavras = palavras.items()
+  palavras = list(palavras)[:10]
+  entidades.append_row[palavras]
