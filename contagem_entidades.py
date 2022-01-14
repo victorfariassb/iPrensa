be5cb72892c8e6ba731f0da783dd581de73cd298
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-14 09:22:35 -0300

    Update contagem_entidades.py

--- contagem_entidades.py
+++ contagem_entidades.py
@@ -29,18 +29,18 @@ nlp.max_length = 2000000
 def conta_entidade(df):
   for site in sites:
     df = pd.DataFrame(site.get_all_records())   
-  text = ''
-  for index, row in df.iterrows():
-    text = text + row['titulo'] + ' '
+    text = ''
+    for index, row in df.iterrows():
+      text = text + row['titulo'] + ' '
         
-  doc = nlp(text)
+      doc = nlp(text)
 
-  palavras = {}
+    palavras = {}
 
-  labels = [x.text for x in doc.ents]
-  dicionario = Counter(labels)
+    labels = [x.text for x in doc.ents]
+    dicionario = Counter(labels)
 
-  palavras = OrderedDict(sorted(dicionario.items(), key = lambda kv : kv[1], reverse=True))
-  palavras = palavras.items()
-  palavras = list(palavras)[:10]
-  entidades.append_row[palavras]
+    palavras = OrderedDict(sorted(dicionario.items(), key = lambda kv : kv[1], reverse=True))
+    palavras = palavras.items()
+    palavras = list(palavras)[:10]
+    entidades.append_row([str(site), palavras])
