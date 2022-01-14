1bb67ad4592547eb92d22db893b2923431f37127
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-14 10:32:56 -0300

    somente funcao

--- contagem_entidades.py
+++ contagem_entidades.py
@@ -1,36 +1,16 @@
-import gspread
-import base64
-import os
-import json
-import pandas as pd
 import spacy
 import pt_core_news_sm
 from collections import Counter, OrderedDict
 
-
-spreadsheet_id = os.environ['GOOGLE_SHEET_ID']
-conteudo_codificado =  os.environ['GOOGLE_SHEETS_CREDENTIALS']
-conteudo = base64.b64decode(conteudo_codificado)
-credentials = json.loads(conteudo)
-
-service_account = gspread.service_account_from_dict(credentials)
-spreadsheet = service_account.open_by_key(spreadsheet_id) 
-
-uol = spreadsheet.worksheet('uol')
-globo = spreadsheet.worksheet('globo')
-jp = spreadsheet.worksheet('jovem_pan')
-entidades = spreadsheet.worksheet('entidades')
-
 nlp = pt_core_news_sm.load()
-nlp.max_length = 2000000
 
 def conta_entidade(df):
-  df = pd.DataFrame(site.get_all_records())   
+  df = pd.DataFrame(df.get_all_records())   
   text = ''
   for index, row in df.iterrows():
     text = text + row['titulo'] + ' '
 
-    doc = nlp(text)
+  doc = nlp(text)
 
   palavras = {}
 
@@ -41,7 +21,4 @@ def conta_entidade(df):
   palavras = palavras.items()
   palavras = list(palavras)[:10]
   entidades.append_row([str(site), palavras])
-  
-  conta_entidade(uol)
-  conta_entidade(globo)
-  conta_entidade(jp)
+ 
