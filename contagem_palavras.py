bf271ead7e032267dcfe68a840dd4b56c2aac181
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-14 13:25:43 -0300

    salvar dados em planilha

--- contagem_palavras.py
+++ contagem_palavras.py
@@ -1,3 +1,5 @@
+import gspread
+
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -5,12 +7,15 @@ from nltk.tokenize import sent_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk import FreqDist
 
-def conta_palavras(arquivo):
+def conta_palavras(base, contagem):
+    linha = 2
+    coluna = 1
+    
     nltk.download('punkt') 
     
     #String text pega todos os títulos do arquivo
     text = ''
-    for index, row in arquivo.iterrows():
+    for index, row in base.iterrows():
         text = text + row['titulo'].lower() + ' ' 
 
     #Sentence tokenizer breaks text paragraph into sentences.
@@ -40,4 +45,14 @@ def conta_palavras(arquivo):
             filtered_sent2.append(w)
     
     fdist = FreqDist(filtered_sent2)
-    return fdist.most_common(10)
+    contagem.update_cell(linha, coluna, fdist[0])
+    contagem.update_cell(linha, coluna + 1, fdist[1])
+    contagem.update_cell(linha, coluna + 2, fdist[2])
+    contagem.update_cell(linha, coluna + 3, fdist[3])
+    contagem.update_cell(linha, coluna + 4, fdist[4])
+    contagem.update_cell(linha, coluna + 5, fdist[5])
+    contagem.update_cell(linha, coluna + 6, fdist[6])
+    contagem.update_cell(linha, coluna + 7, fdist[7])
+    contagem.update_cell(linha, coluna + 8, fdist[8])
+    contagem.update_cell(linha, coluna + 9, fdist[9])
+    linha += 1
