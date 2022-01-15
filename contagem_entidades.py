06c0923ff597aabb99b68e3f412a9def212acbab
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-15 18:14:38 -0300

    Update contagem_entidades.py

--- contagem_entidades.py
+++ contagem_entidades.py
@@ -5,20 +5,15 @@ from collections import Counter, OrderedDict
 nlp = pt_core_news_sm.load()
 
 def conta_entidade(df):
-  df = pd.DataFrame(df.get_all_records())   
-  text = ''
-  for index, row in df.iterrows():
-    text = text + row['titulo'] + ' '
+  df = pd.DataFrame(df.get_all_records())
+  df = df['titulo']
+  text = ' '.join(df.col_values(1))
 
   doc = nlp(text)
-
-  palavras = {}
 
   labels = [x.text for x in doc.ents]
   dicionario = Counter(labels)
 
-  palavras = OrderedDict(sorted(dicionario.items(), key = lambda kv : kv[1], reverse=True))
-  palavras = palavras.items()
-  palavras = list(palavras)[:10]
+  palavras = [palavra for palavra, contagem in dicionátio.most_common(10)]
   entidades.append_row([str(site), palavras])
  
