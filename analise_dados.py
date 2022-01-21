3f21fd2c5e872898837cf9a0a4bf4c029198fdbf
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-21 10:50:13 -0300

    create analise dados
    mudei de lugar parte da automação para que a API do sheets não colapse

--- analise_dados.py
+++ analise_dados.py
@@ -1,13 +1,7 @@
 import base64
 import gspread
 import json
-import pandas as pd
-import pytz
 import os
-import requests
-import re
-import time
-from datetime import datetime
 
 from contagem_candidatos import contagem_candidatos
 from contagem_palavras import conta_palavras
@@ -20,21 +14,27 @@ credentials = json.loads(conteudo)
 service_account = gspread.service_account_from_dict(credentials) # autenticação
 spreadsheet = service_account.open_by_key(spreadsheet_id) #abrir arquivo
 
+# Abre as planilhas
 globo_sheet = spreadsheet.worksheet('globo') 
 uol_sheet = spreadsheet.worksheet('uol') 
 jp_sheet = spreadsheet.worksheet('jovem_pan') 
 folha_sheet = spreadsheet.worksheet('folha') 
 oglobo_sheet = spreadsheet.worksheet('oglobo') 
+contagem_candidato = spreadsheet.worksheet('contagem_candidato')
+
+
+# Contagem de candidatos
+contagem_candidatos(globo_sheet, contagem_candidato)
+contagem_candidatos(uol_sheet, contagem_candidato)
+contagem_candidatos(jp_sheet, contagem_candidato)
+contagem_candidatos(folha_sheet, contagem_candidato)
+contagem_candidatos(oglobo_sheet, contagem_candidato)
 
 
 # Mais faladas
-contagem_candidato = spreadsheet.worksheet('contagem_candidato')
-contagem_candidatos(globo_sheet, contagem_candidato)
-
-contagem_candidatos(uol_sheet, contagem_candidato)
-
-contagem_candidatos(jp_sheet, contagem_candidato)
-
-contagem_candidatos(folha_sheet, contagem_candidato)
-
-contagem_candidatos(oglobo_sheet, contagem_candidato)
+mais_faladas = spreadsheet.worksheet('mais_faladas')
+conta_palavras(globo_sheet, mais_faladas)
+conta_palavras(uol_sheet, mais_faladas)
+conta_palavras(jp_sheet, mais_faladas)
+conta_palavras(folha_sheet, mais_faladas)
+conta_palavras(oglobo_sheet, mais_faladas)
