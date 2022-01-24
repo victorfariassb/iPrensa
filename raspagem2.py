ac5472d468a0b328b7ec4dab822162cce492895b
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-24 18:20:14 -0300

    Create raspagem2.py

---
+++ raspagem2.py
@@ -0,0 +1,25 @@
+import base64
+import gspread
+import json
+import pandas as pd
+import os
+
+from raspador_sites import coleta_folha, coleta_oglobo
+
+spreadsheet_id = os.environ['GOOGLE_SHEET_ID']
+conteudo_codificado =  os.environ['GOOGLE_SHEETS_CREDENTIALS']
+conteudo = base64.b64decode(conteudo_codificado)
+credentials = json.loads(conteudo)
+
+service_account = gspread.service_account_from_dict(credentials) 
+spreadsheet = service_account.open_by_key(spreadsheet_id) 
+
+oglobo_sheet = spreadsheet.worksheet('oglobo') 
+folha_sheet = spreadsheet.worksheet('folha') 
+
+try:
+  coleta_folha()
+finally:
+  next
+
+coleta_oglobo()
