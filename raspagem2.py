eb95300e0b0a048ee4fe2dfd4265ec28a224e8c7
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-31 09:59:45 -0300

    add estadao

--- raspagem2.py
+++ raspagem2.py
@@ -16,16 +16,25 @@ spreadsheet = service_account.open_by_key(spreadsheet_id)
 
 folha_sheet = spreadsheet.worksheet('folha') 
 oglobo_sheet = spreadsheet.worksheet('oglobo') 
+estadao_sheet = spreadsheet.worksheet('estadao')
 
 try:
   coleta_folha(folha_sheet)
 finally:
   next
+try:
+  coleta_oglobo(oglobo_sheet)
+finally:
+  next
 
-coleta_oglobo(oglobo_sheet)
+  try:
+    coleta_estadao(estadao_sheet)
+finally:
+  next
 
 # Conta palavras 
 contagem = spreadsheet.worksheet('mais_faladas') 
 
 conta_palavras(folha_sheet, contagem)
 conta_palavras(oglobo_sheet, contagem)
+conta_palavras(estadao_sheet, contagem)
