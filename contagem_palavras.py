e58f15a25537d1017d25f603671b1f5240b42987
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-01-31 22:00:58 -0300

    add words to stopwords

--- contagem_palavras.py
+++ contagem_palavras.py
@@ -46,7 +46,7 @@ def conta_palavras(base, contagem):
     stop_words = set(stopwords.words("portuguese"))
     for x in ['seção','@','#',',', '!', ':', 'vídeo', 'quer', 'uol', 'vai', 'carros', 'pode', 'novo', 'afirma', '2021', 'confira', 'durante', '.', 'a', 'sobre', 'diz', 'após', 'veja', 'ser', 'faz', 'ex', 'maior', '22',
               'r', '1', '4', '9', '2', '3', '5', '6', '7', '8', 'anos', 'ano', '2022', 'dia', 'contra', 'virada', 'melhores', 'mil', '19', 'fotos', 'foto', 'fazer', 'pede', 'momento', 'mostra', 'pede', 'momento', '0',
-             'conheça']:
+             'conheça', 'deve', 'hoje']:
         stop_words.add(x)
 
     tokenized_sent = tokenized_word
