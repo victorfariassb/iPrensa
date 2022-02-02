828d95149334d5135a4f666e554979c710fe9ed5
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-02-02 14:17:03 -0300

    tirar uol

--- raspagem1.py
+++ raspagem1.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 import os
 
-from raspador_sites import pega_editoria, pega_localizacao, pega_link, coleta_globo, coleta_uol, coleta_jp, coleta_folha, coleta_oglobo
+from raspador_sites import pega_editoria, pega_localizacao, pega_link, coleta_globo, ccoleta_jp, coleta_folha, coleta_oglobo
 from contagem_palavras import conta_palavras
 
 spreadsheet_id = os.environ['GOOGLE_SHEET_ID']
@@ -16,7 +16,6 @@ service_account = gspread.service_account_from_dict(credentials)
 spreadsheet = service_account.open_by_key(spreadsheet_id) 
 
 globo_sheet = spreadsheet.worksheet('globo') 
-uol_sheet = spreadsheet.worksheet('uol') 
 jp_sheet = spreadsheet.worksheet('jovem_pan') 
 
 try:
@@ -24,11 +23,6 @@ try:
 finally:
   next
   
-try:
-  coleta_uol(uol_sheet)
-finally:
-  next
-
 coleta_jp(jp_sheet)
 
 
@@ -36,5 +30,4 @@ coleta_jp(jp_sheet)
 contagem = spreadsheet.worksheet('mais_faladas') 
 
 conta_palavras(globo_sheet, contagem)
-conta_palavras(uol_sheet, contagem)
 conta_palavras(jp_sheet, contagem)
