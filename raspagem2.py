6b7570bdae85e4eb2a17771fc6ff5775edbfb7bd
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-02-03 11:46:38 -0300

    add contagem candidatos

--- raspagem2.py
+++ raspagem2.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 import os
 
+from contagem_candidatos import contagem_candidatos
 from contagem_palavras import conta_palavras
 from raspador_sites import coleta_folha, coleta_oglobo, coleta_estadao
 
@@ -19,10 +20,27 @@ folha_sheet = spreadsheet.worksheet('folha')
 oglobo_sheet = spreadsheet.worksheet('oglobo') 
 estadao_sheet = spreadsheet.worksheet('estadao')
 
+# Conta palavras 
+contagem = spreadsheet.worksheet('mais_faladas') 
+
+conta_palavras(folha_sheet, contagem)
+conta_palavras(oglobo_sheet, contagem)
+conta_palavras(estadao_sheet, contagem)
+
+# Conta candidatos
+contagem_candidato = spreadsheet.worksheet('contagem_candidato')
+
+contagem_candidatos(folha_sheet, contagem_candidato)
+contagem_candidatos(oglobo_sheet, contagem_candidato)
+contagem_candidatos(estadao_sheet, contagem_candidato)
+
+# Raspagem de dados
+
 try:
   coleta_folha(folha_sheet)
 finally:
   next
+  
 try:
   coleta_oglobo(oglobo_sheet)
 finally:
@@ -33,9 +51,3 @@ try:
 finally:
   next
 
-# Conta palavras 
-contagem = spreadsheet.worksheet('mais_faladas') 
-
-conta_palavras(folha_sheet, contagem)
-conta_palavras(oglobo_sheet, contagem)
-conta_palavras(estadao_sheet, contagem)
