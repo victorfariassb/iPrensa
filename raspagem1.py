391100f049bbbf815a1b54b3028e6f419de5d402
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-02-03 11:45:33 -0300

    add contagem de candidatos

--- raspagem1.py
+++ raspagem1.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 import os
 
+from contagem_candidatos import contagem_candidatos
 from raspador_sites import pega_editoria, pega_localizacao, pega_link, coleta_globo, coleta_jp
 from contagem_palavras import conta_palavras
 
@@ -18,6 +19,19 @@ spreadsheet = service_account.open_by_key(spreadsheet_id)
 globo_sheet = spreadsheet.worksheet('globo') 
 jp_sheet = spreadsheet.worksheet('jovem_pan') 
 
+# Conta palavras 
+contagem = spreadsheet.worksheet('mais_faladas') 
+
+conta_palavras(globo_sheet, contagem)
+conta_palavras(jp_sheet, contagem)
+
+# Conta candidatos
+contagem_candidato = spreadsheet.worksheet('contagem_candidato')
+
+contagem_candidatos(globo_sheet, contagem_candidato)
+contagem_candidatos(jp_sheet, contagem_candidato)
+
+# Raspa os dados
 try:
   coleta_globo(globo_sheet)
 finally:
@@ -26,8 +40,4 @@ finally:
 coleta_jp(jp_sheet)
 
 
-# Conta palavras 
-contagem = spreadsheet.worksheet('mais_faladas') 
 
-conta_palavras(globo_sheet, contagem)
-conta_palavras(jp_sheet, contagem)
