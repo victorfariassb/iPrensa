193d343e6ea975ceee46726d7b3287c909ef6ba3
victorfariassb <57238563+victorfariassb@users.noreply.github.com>
2022-02-03 11:47:52 -0300

    add contagem candidatos

--- raspagem3.py
+++ raspagem3.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 import os
 
+from contagem_candidatos import contagem_candidatos
 from raspador_sites import coleta_uol
 from contagem_palavras import conta_palavras
 
@@ -16,13 +17,22 @@ service_account = gspread.service_account_from_dict(credentials)
 spreadsheet = service_account.open_by_key(spreadsheet_id) 
 
 uol_sheet = spreadsheet.worksheet('uol') 
-  
-try:
-  coleta_uol(uol_sheet)
-finally:
-  next
+
 
 # Conta palavras 
 contagem = spreadsheet.worksheet('mais_faladas') 
 
 conta_palavras(uol_sheet, contagem)
+
+# Conta candidatos
+contagem_candidato = spreadsheet.worksheet('contagem_candidato')
+
+contagem_candidatos(uol_sheet, contagem_candidato)
+
+# Raspa os dados
+try:
+  coleta_uol(uol_sheet)
+finally:
+  next
+
+
