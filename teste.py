ff1b91cf02c80891ca06c3c404835db1efd358ee
Victor Farias <57238563+victorfariassb@users.noreply.github.com>
2022-06-25 15:15:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -16,9 +16,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
-options.add_argument('--disable-gpu')
+options.add_argument("start-maximized")
 options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
 options.binary_location = os.environ["GOOGLE_CHROME_PATH"]
 browser = webdriver.Chrome(service=Service, options=options, executable_path=ChromeDriverManager().install())
 
