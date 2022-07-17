a2b342366138b68b4394c1308d4d22a35aed2ad8
victorfariassb <victorfariassb@gmail.com>
2022-07-17 20:59:51 -0300

    building sobre

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+let bttn_sobre = document.querySelector('.sobre');
+let sobre = document.querySelectorAll('.home > div:not(.logo)');
+let logo = document.querySelector('.logo');
 
 
+bttn_sobre.onclick = function() {
+    for (let item of sobre) {
+    item.style.display = 'none';
+    }
+    logo.style.gridColumnEnd = 7;
+    logo.style.gridRowEnd = 7;
+}
