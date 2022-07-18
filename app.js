ddbc8b41575a2f722e60dd4eaad73ec26ff0cd71
victorfariassb <victorfariassb@gmail.com>
2022-07-18 09:40:00 -0300

    building sobre

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-let bttn_sobre = document.querySelector('.sobre');
-let sobre = document.querySelectorAll('.home > div:not(.logo)');
+let bttn_sobre = document.querySelector('.bttn-sobre');
+let bttn_voltar = document.querySelector('.bttn-voltar');
+let main = document.querySelectorAll('.home > div:not(.logo)');
+let home = document.querySelector('.home')
+let intro = document.querySelector('.intro')
 let logo = document.querySelector('.logo');
+let sobre = document.querySelector('.sobre')
 
-
-bttn_sobre.onclick = function() {
-    for (let item of sobre) {
-    item.style.display = 'none';
+// página se transforma de modo a apagar dados das outras editorias e mostrar apenas o sobre
+bttn_sobre.onclick = function () {
+    for (let item of main) {
+        item.style.display = 'none';
     }
-    logo.style.gridColumnEnd = 7;
-    logo.style.gridRowEnd = 7;
+    home.style.display = 'inline-block';
+    logo.style.display = 'flex';
+
+    intro.style.borderRight = '2px solid #000326'
+    bttn_sobre.style.display = 'none';
+    bttn_voltar.style.display = 'inline-block';
+    sobre.style.display = 'block';
+    logo.style.flexDirection = 'row';
+}
+
+// aqui criamos o mecanismo para voltar à página inicial
+bttn_voltar.onclick = function () {
+    window.location.reload();
 }
