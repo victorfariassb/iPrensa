9386a34cc65cf116ef8136e137cfb590e5a3de85
victorfariassb <victorfariassb@gmail.com>
2022-08-09 23:14:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ quantidade_ultima_semana = ranking_candidatos.col_values(3)[25:]
 quantidade_total_candidatos = ranking_candidatos.col_values(5)[25:]
 
 ranking_candidatos = pd.DataFrame(list(zip(candidatos, quantidade_ultima_semana, quantidade_total_candidatos)), columns=['candidato', 'quantidade_ultima_semana', 'quantidade_total_candidatos'])
-ranking_candidatos.quantidade_total_candidatos = ranking_candidatos.quantidade_total_candidatos.astype(int)
 ranking_candidatos = ranking_candidatos.sort_values('quantidade_total_candidatos', ascending=False)
 
 
